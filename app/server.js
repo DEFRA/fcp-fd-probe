a3b4ae83027f06f1787290e5d55f6f34e61a1161
Amirs Ismuhametovs <98330195+JungaBunga@users.noreply.github.com>
2024-09-18 13:35:58 +0100

    initial commit (#1)

--- app/server.js
+++ app/server.js
@@ -1,26 +1,28 @@
 import Hapi from '@hapi/hapi'
+import HapiPino from 'hapi-pino'
 import Joi from 'joi'
-import { registerPlugins } from './plugins/index.js'
-import config from './config.js'
+import healthy from './routes/healthy.js'
+import healthz from './routes/healthz.js'
 
-async function createServer () {
+const createServer = () => {
   const server = Hapi.server({
-    host: config.get('host'),
-    port: config.get('port'),
-    routes: {
-      validate: {
-        options: {
-          abortEarly: false
-        }
-      }
-    },
-    router: {
-      stripTrailingSlash: true
-    }
+    port: process.env.PORT
   })
 
+  const routes = [].concat(
+    healthy,
+    healthz
+  )
+
   server.validator(Joi)
-  await registerPlugins(server)
+  server.route(routes)
+  server.register({
+    plugin: HapiPino,
+    options: {
+      logPayload: true,
+      level: 'warn'
+    }
+  })
 
   return server
 }
